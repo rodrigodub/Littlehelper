9509b06c9ee555f41424f6cd5d7d26ee148a9f96
rodrigodub <nobrega@gmail.com>
2015-07-13 16:51:25 +0800

    Started development
    # v0.1.001

---
+++ src/Littlehelper.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python3
+
+#################################################
+#                   Littlehelper
+# Little Helper is just an utility module for
+# database and text methods
+#
+# v0.1.001
+# for Issue #
+#
+# Rodrigo Nobrega
+# 20150713-
+#################################################
+__author__ = 'Rodrigo Nobrega'
+
+# import modules
+# import os
+# import datetime
+
+
+# PkGetDirectory()
+class Rodrigo1(object):
+    """
+    Class to do something
+    """
+    def __init__(self):
+        pass
+
+
+# test loop
+def test():
+    print('------------------------')
+    print('Test.')
+    print('------------------------')
+
+
+# main loop
+def main():
+    print('------------------------')
+    print('Main.')
+    print('------------------------')
+
+
+# main, calling main loop
+if __name__ == '__main__':
+    test()
+    # main()
